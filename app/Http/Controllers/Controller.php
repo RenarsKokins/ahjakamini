152d0857c9dbe4ed49ba39a61d26d9f7ab415190
Renars <kokins.renars@gmail.com>
2022-03-02 03:34:06 +0200

    created new controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\View;
+use App\Models\Language;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        $languages = Language::all();
+        View::share('languages', $languages);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\App;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\PriceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,18 +23,9 @@ use App\Http\Controllers\ProductController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products', [ProductController::class, 'index'])->name('products');
 Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery');
-
-Route::get('/about', function() {
-    return view('about');
-})->name('about');
-
-Route::get('/contacts', function() {
-    return view('contacts');
-})->name('contacts');
-
-Route::get('/price', function() {
-    return view('price');
-})->name('price');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/contacts', [ContactsController::class, 'index'])->name('contacts');
+Route::get('/price', [PriceController::class, 'index'])->name('price');
 
 Route::get('/locale/{locale}', function ($locale) {
     session(['locale'=> strtolower($locale)]);
